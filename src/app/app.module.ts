01b6926fb8465fe8ef97dfd53520ac263ed69aa5
dagiraldova <danielgvallejo1997@gmail.com>
2018-11-22 11:29:18 -0500

    Se crea la rama home para el manejo los componentes y estilos de la pagina principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
