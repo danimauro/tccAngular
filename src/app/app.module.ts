2301a818cc4dfbb2f594ef76dcfb21867a4f1179
danimauro <dmgutierrez7@misena.edu.co>
2018-11-23 11:39:30 -0500

    Se add @media-query en estilos del c home, se add muted al video inicia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+//Shared
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { HomeComponent } from './components/shared/home/home.component';
