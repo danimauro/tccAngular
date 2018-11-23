c726ddb14d5ce5ee089c74b20fce29a3917a725c
dagiraldova <danielgvallejo1997@gmail.com>
2018-11-23 10:06:38 -0500

    Se agrega home y estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomeComponent } from './components/shared/home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
